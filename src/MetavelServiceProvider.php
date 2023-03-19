d3fa4d7169e581cdcbbaa48da0286344482ee4cf
Tom benevides <elton.benevides@refuturiza.com.br>
2023-03-19 01:21:46 -0400

    include config publish

--- src/MetavelServiceProvider.php
+++ src/MetavelServiceProvider.php
@@ -10,6 +10,10 @@ class MetavelServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadViewComponentsAs('metavel', $this->viewComponents());
+
+        $this->publishes([
+            __DIR__ . '/config/metavel.php' => config_path('metavel.php'),
+        ], 'metavel-config');
     }
 
     public function register(): void
